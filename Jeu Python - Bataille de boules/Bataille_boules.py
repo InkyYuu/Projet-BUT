65e493701d299ea28224f2ea3cab0213c6e6bd11
Inky <94309802+InkyYuu@users.noreply.github.com>
2022-10-16 10:57:14 +0200

    Premiers fichiers du jeu

---
+++ Jeu Python - Bataille de boules/Bataille_boules.py
@@ -0,0 +1,28 @@
+# =========================== IMPORTS ================================
+from upemtk import *
+from math import *
+from random import *
+from time import *
+# ========================== FONCTIONS ===============================
+def choix_couleur ():
+    pass 
+def pose_ronds () :
+    pass
+def calcul_score ():
+    pass
+def autre ():
+    pass
+# ========================== VARIABLES ===============================
+victoire = False
+variant_sablier = False
+variant_score = False
+variant_taille_boule = False
+variant_dynamique = False
+variant_terminaisonc = False
+variant_obstacles = False
+hauteurFenetre = 1080
+largeurFenetre = 1920
+# ========================= CODE PRINCIPAL ===========================
+cree_fenetre(largeurFenetre, hauteurFenetre)
+attente_clic()
+ferme_fenetre()
