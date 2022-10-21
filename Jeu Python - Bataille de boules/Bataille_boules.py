3b6dde2089f5e2a8ef5fbe2d067125544ec9e4fd
Inky <94309802+InkyYuu@users.noreply.github.com>
2022-10-21 17:10:32 +0200

    Nouvel essai

--- Jeu Python - Bataille de boules/Bataille_boules.py
+++ Jeu Python - Bataille de boules/Bataille_boules.py
@@ -1,28 +1,96 @@
 # =========================== IMPORTS ================================
+from hashlib import blake2b
+from tkinter import CENTER
 from upemtk import *
 from math import *
 from random import *
 from time import *
+# ======================= NOS MODULES A NOUS ======================
+from COLORS import choixcouleur
 # ========================== FONCTIONS ===============================
-def choix_couleur ():
-    pass 
-def pose_ronds () :
-    pass
+def pose_ronds (liste_boule_allie,liste_boule_ennemi, couleur_allie, couleur_ennemi, tour, rayon) :
+    """
+    Arguments :
+    liste_boule_allie sous forme [Ox,Oy,rayon,tag]
+    liste_boule_ennemi sous forme [Ox,Oy,rayon,tag]
+    couleur_allie est un string de la couleur de l'équipe allié
+    couleur_ennemi est un string de la couleur de l'équipe ennemi
+    tour représente à quel tour nous somme
+    rayon est le rayon de la boule que nous allons posée
+    """
+    Ox, Oy, RIEN = attente_clic()
+    Cas1 = False
+    Cas2 = False
+    Cas3 = False
+    if liste_boule_allie == [] and liste_boule_ennemi == []:
+        cercle(Ox,Oy,rayon,'black',couleur_allie,tag='boule'+couleur_allie+str(tour))
+        liste_boule_allie.append([Ox,Oy,rayon,'boule'+couleur_allie+str(tour)])
+    else :
+        for j in range(len(liste_boule_allie)):
+            if sqrt((liste_boule_allie[j][0]-Ox)**2 + (liste_boule_allie[j][1]-Oy)**2) < liste_boule_allie[j][2] :
+                return
+            elif sqrt((liste_boule_allie[j][0]-Ox)**2 + (liste_boule_allie[j][1]-Oy)**2) < liste_boule_allie[j][2] + rayon:
+                return
+        for i in range(len(liste_boule_ennemi)):
+            print(liste_boule_ennemi[i])
+            if sqrt((liste_boule_ennemi[i][0]-Ox)**2 + (liste_boule_ennemi[i][1]-Oy)**2) < liste_boule_ennemi[i][2] :
+                Cas3 = True
+                print('Cas3')
+                indice = i
+                break
+            elif sqrt((liste_boule_ennemi[i][0]-Ox)**2 + (liste_boule_ennemi[i][1]-Oy)**2) < liste_boule_ennemi[i][2] + rayon:
+                Cas2 = True
+                print('Cas2')
+                break
+        if Cas2 == False and Cas3 == False :
+            Cas1 = True
+        if Cas1 == True :
+            cercle(Ox,Oy,rayon,'black',couleur_allie,tag='boule'+couleur_allie+str(tour))
+            liste_boule_allie.append([Ox,Oy,rayon,'boule'+couleur_allie+str(tour)])
+        elif Cas2 == True :
+            print(('Faux'))
+            #texte(0,0,"IMPOSSIBLE !", 'black', 'center')
+        elif Cas3 == True :
+            #efface(liste_boule_ennemi[indice][3])
+            rayon_allie = liste_boule_ennemi[indice][2] - sqrt((liste_boule_ennemi[indice][0]-Ox)**2 + (liste_boule_ennemi[indice][1]-Oy)**2)
+            rayon_ennemi = liste_boule_ennemi[indice][2]-rayon_allie
+            cercle(Ox,Oy,rayon_allie,'black',couleur_allie,tag='boule'+couleur_allie+str(tour))
+            cercle(Ox,Oy,rayon_ennemi,'black',couleur_ennemi,tag='boule'+couleur_ennemi+str(tour))
+            liste_boule_ennemi.append([Ox,Oy,rayon_ennemi,'boule'+couleur_ennemi+str(tour)])
+            liste_boule_allie.append([Ox,Oy,rayon_allie,'boule'+couleur_allie+str(tour)])
+            liste_boule_ennemi.pop(indice)
+    mise_a_jour()
+   
 def calcul_score ():
     pass
+
 def autre ():
     pass
+
 # ========================== VARIABLES ===============================
+
 victoire = False
 variant_sablier = False
 variant_score = False
 variant_taille_boule = False
 variant_dynamique = False
-variant_terminaisonc = False
+variant_terminaison = False
 variant_obstacles = False
-hauteurFenetre = 1080
-largeurFenetre = 1920
+hauteurFenetre = 540
+largeurFenetre = 960
 # ========================= CODE PRINCIPAL ===========================
+couleurJ1 = choixcouleur(300,300)
+couleurJ2 = choixcouleur(300,300)
 cree_fenetre(largeurFenetre, hauteurFenetre)
-attente_clic()
-ferme_fenetre()
+tour = 0
+lst_boule_J1 = []
+lst_boule_J2 = []
+while victoire != True :
+    for joueur in range (2):
+        if not(joueur%2) :
+            pose_ronds (lst_boule_J1,lst_boule_J2, couleurJ1, couleurJ2, tour, 50)
+        else :
+            pose_ronds (lst_boule_J2,lst_boule_J1, couleurJ2, couleurJ1, tour, 50)
+    tour += 1
+    if tour == 5 :
+        victoire = True
--- Jeu Python - Bataille de boules/COLORS.py
+++ Jeu Python - Bataille de boules/COLORS.py
@@ -1,44 +1,93 @@
 from cgitb import text
+from telnetlib import NOOPT
 from upemtk import *
 from random import *
 from time import *
 from math import *
+from PIL import *
 
-if __name__ == '__main__':
-    largeurFenetre = 300
-    hauteurFenetre = 300
-    lstcolo=['red','blue','green','cyan','pink','purple','yellow','orange']
-    a = 0
-
+def choixcouleur(largeurFenetre,hauteurFenetre):
     cree_fenetre(largeurFenetre, hauteurFenetre)
-    texte(0,0,"Choississez votre couleur d'équipe : ",taille=14)
-    texte(0,280,"Appuyez sur Space pour Sortir",taille=10)
-    #FLECHE GAUCHE
-    ligne(50,125,25,150,'black',3)
-    ligne(24,150,50,175,'black',3)
-    texte(15,200,"Gauche",taille=14)
-    #FLECHE DROITE
-    ligne(250,125,275,150,'black',3)
-    ligne(276,150,250,175,'black',3)
-    texte(225,200,"Droite",taille=14)
+#=================== VARIABLES ET LISTES DE BASE ==================================================
+    lstcolo=['red','blue','green','cyan','pink','purple','yellow','orange']
+    lstcolofr=['rouge','bleu','vert','cyan','rose','violet','jaune','orange']
+    a = 0 #a c'est l'indice de la position dans la liste (voir en dessous)
+
+#============CREATION TRUC DE BASE DESSIN ========================================================
     
+    #C'EST LE FONC D'ECRAN
+    #image(100,100,'G:\SAE 1.1\Mes brouillons\shiny.png')
+
+    #CEST LE TEXTE D'INFORMATION
+    texte(0,0,"Choississez votre couleur d'équipe : ",taille=14)
+
+    #FLECHE GAUCHE
+    ligne(50,125,25,150,'black',3) #   /
+    ligne(24,150,50,175,'black',3) #   \
+    texte(15,185,"Gauche",taille=14)
+    
+    #FLECHE DROITE
+    ligne(250,125,275,150,'black',3)  # \
+    ligne(276,150,250,175,'black',3)  # /
+    texte(225,185,"Droite",taille=14)
+    
+#============ MES AUTRES VARIABLES ================================================================
     maxcolo = len(lstcolo)
+    couleurT1 = None
     #print(maxcolo)
 
-    while True :
+#============ MON WHILE ================================================================
+    # dans la doc c'est while true mais je fais arreter mon programme après selection de la couleur d'équipe dans couleurT1 (couleurTeam1)
+    while couleurT1 is None :
+
+        #Truc pr faire des événement de la doc
         evenement = donne_evenement()
-        type_ev = type_evenement(evenement)
+        type_ev = type_evenement(evenement) #type = touche ou clic
+
+        #Le ROND AU MILIEU
         cercle((largeurFenetre/2),(hauteurFenetre/2),50,'black',lstcolo[a],3)
-        if type_ev == 'Touche':
+
+        #RAJOUT DALTONIEN
+        efface('lol')
+        texte(150,75,lstcolofr[a],lstcolo[a],"center",tag='lol')
+
+        #BOUTON DE SELECTION
+        rectangle(65,225,235,265,'black','light gray')
+        texte(150,245,"ENTER",ancrage='center')
+        
+
+#============ LES TOUCHES ================================================================
+        if type_ev == 'Touche': #Dire le type de evenement (touche ou clic)
             nom_touche = touche(evenement)
             if nom_touche == 'Left':
-                a -= 1
+                a -= 1 
+        # a c'est pour rappel l'indice dans la position de la liste donc si a change, la couleur aussi
                 #print(a)
             if nom_touche == 'Right':
                 a += 1
                 #print(a)
-            if nom_touche == 'space': 
+            if nom_touche == 'Return': 
+        #RETURN CEST ENTRER
+                couleurT1 = lstcolo[a] #DONC SI ON CLIQUE ON CHOISIT LA COULEUR PR LA TEAM
+                affichagecoloT1 = lstcolofr[a]
                 ferme_fenetre()
+                return couleurT1
+                print("couleur séléctionnée de l'équipe 1 :",affichagecoloT1)
             if a == maxcolo or a == -(maxcolo) :
                 a = 0
-        mise_a_jour() 
+        #ce if c'est pour éviter le out of range
+
+        if type_ev == "ClicGauche": #Dire le type de evenement (touche ou clic)
+                x = int(clic_x(evenement)) #On note les co de x
+                y = int(clic_y(evenement)) #On note les co de y
+                #print((clic_x(evenement)),(clic_y(evenement)))
+                if 75<x<225 and 220<y<275: #Dimension du bouton et if si le clic est dedans
+                    couleurT1 = lstcolo[a] #DONC SI ON CLIQUE ON CHOISIT LA COULEUR PR LA TEAM
+                    #Confirmation
+                    affichagecoloT1 = lstcolofr[a]
+                    ferme_fenetre()
+                    return couleurT1
+                    print("couleur séléctionnée de l'équipe 1 :",affichagecoloT1)
+    
+        mise_a_jour()
+    
